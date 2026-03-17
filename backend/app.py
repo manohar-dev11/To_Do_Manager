5df9cd1e407837b34399e9b4320a214bb638ef4e
BaluYellulla <baluyellulla12345@gmail.com>
2026-03-17 15:01:20 +0530

    Implemented Flask backend and frontend API integration

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/")
-def home():
-    return "Smart Task Manager API Running"
+tasks = []
+
+@app.route("/tasks", methods=["GET"])
+def get_tasks():
+    return jsonify(tasks)
+
+@app.route("/tasks", methods=["POST"])
+def add_task():
+    data = request.json
+    tasks.append(data)
+    return jsonify({"message": "Task added"})
+
+@app.route("/tasks/<int:task_id>", methods=["DELETE"])
+def delete_task(task_id):
+    global tasks
+    tasks = [t for t in tasks if t["id"] != task_id]
+    return jsonify({"message": "Task deleted"})
 
 if __name__ == "__main__":
     app.run(debug=True)
